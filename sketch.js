2c7daba956020f29a0663571dc62d530387fe793
tiaoffthewall <mattiabonanno@outlook.com>
2022-11-22 13:55:36 +0100

    Edits

--- sketch.js
+++ sketch.js
@@ -1,16 +1,55 @@
-function preload() {
-	// put preload code here
-}
+//I did followed a tutorial to solve the assignment because I found it pretty complex to solve it alone.
+//https://www.youtube.com/watch?v=AbB9ayaffTc&list=WL&index=1&t=12s
+//I also had lots of problems with localhost and ngrok 
+
+let permissionGranted = false;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  if (typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
+    
+    DeviceOrientationEvent.requestPermission()
+    .catch(() => {
+      let button = createButton("click to allow access to sensors");
+      button.style("font-size", "24px");
+      button.center();
+      button.mousePressed(requestAccess);
+      throw error;
+    })
+    .then(() => {
+      permissionGranted = true;
+    })
+  }else {
+    textSize(48);
+    text("non ios device", 100, 100);
+  }
+  
+}
+
+function requestAccess(){
+  DeviceOrientationEvent.requestPermission()
+  .then(response => {
+    if(response=='granted'){
+      permissionGranted = true;
+    }else{
+      persmissionGranted = false;
+    }
+  })
+  .catch(console.error);
+  this.remove();
 }
 
 function draw() {
-	// put drawing code here
+  if (!permissionGranted) return;
+  //background(220);
+  
+  const dx = constrain(rotationY, -3, 3);
+  const dy = constrain(rotationX, -3, 3);
+  cx += dx*2;
+  cy += dy*2;
+  cx = constrain(cx, 0, width);
+  cy = constrain(cy, 0, height);
+  
+  ellipse(cx, cy, 200, 200);
+  
 }
